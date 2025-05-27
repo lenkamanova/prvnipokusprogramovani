efdf70774573564c5b255fe4a1afe005c7e2de93
lenkamanova <lenka.manova@volny.cz>
2025-05-27 19:06:09 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Toto je pro vypsani textu
+
+        System.out.println("Ahoj svete!");
+        System.out.println("Ahoj svete!");
+        System.out.println("Ahoj svete!");
+
+    }
+}
