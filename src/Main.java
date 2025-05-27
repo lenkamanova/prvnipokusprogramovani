536e8b59f4a6e17bf8ace856dff01223d235125f
lenkamanova <lenka.manova@volny.cz>
2025-05-27 19:38:51 +0200

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+       /* String pozdrav = "Ahoj svete";
 
         //Toto je pro vypsani textu
 
         System.out.println("Ahoj svete!");
         System.out.println("Ahoj svete!");
-        System.out.println("Ahoj svete!");
+        System.out.println("Ahoj svete!");*/
+
+        int prvniCislo = 10;
+        int druheCislo = 5;
+
+
+
+        System.out.println(prvniCislo + druheCislo);
+        System.out.println(prvniCislo - druheCislo);
+        System.out.println(prvniCislo * druheCislo);
+        System.out.println(prvniCislo / druheCislo);
+
+        String pozdrav2 = "Ahoj";
+        String jmeno = "Honza";
+
+        System.out.println(pozdrav2 + jmeno);
+        //Uloha: Uzivatel napise sve jmeno a program ho pozdravi
+        String pozdrav3 = "Ahoj, ";
+        Scanner mujScanner = new Scanner(System.in);
+        System.out.println("Zadej sve jmeno");
+        String jmeno = mujScanner.nextLine();
+        System.out.println(pozdrav3 + jmeno);
+
+
 
     }
 }
