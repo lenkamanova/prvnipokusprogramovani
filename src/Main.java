9450e6dc96d931d8cf327cd54dda13b828ccf18e
lenkamanova <lenka.manova@volny.cz>
2025-06-03 18:52:45 +0200

    First commit

--- src/Main.java
+++ src/Main.java
@@ -218,7 +218,7 @@ public class Main {
             System.out.println(i);
 
         }*/
-        String [] jmena = new String[] {"Petr", "Jan", "Katka", "Eva", "Jana"};
+        /*String [] jmena = new String[] {"Petr", "Jan", "Katka", "Eva", "Jana"};
 
         //for
         for (int i = 0; i < jmena.length; i++){
@@ -228,7 +228,43 @@ public class Main {
         //foreach
         for (String jmeno: jmena){
             System.out.println(jmeno);
+        }*/
+
+        //while
+        /*int ridiciPromenna = 0;
+        while (ridiciPromenna < 5){
+            System.out.println(ridiciPromenna);
+            ridiciPromenna++;
         }
+        System.out.println("Tady pokracuju");*/
+
+        //do...while (provede se mi minimalne jednou bez ohledu na podminku
+        /*int ridiciPromena2 = 0;
+        do {
+            System.out.println(ridiciPromena2);
+            ridiciPromena2++;
+        } while (ridiciPromena2 == 5);
+        System.out.println("Tady pokracuju");*/
+
+        //Vylepseni ulohy hodu kostkami
+        int hozeneCislo;
+        Random nahodnyGenerator = new Random();
+        boolean ridiciPromenna = true;
+
+        do {
+        System.out.println("Pro hozeni kostkou stiskni Enter");
+        Scanner mujScanner = new Scanner(System.in);
+        mujScanner.nextLine();
+
+        hozeneCislo = nahodnyGenerator.nextInt(1,7);
+        System.out.println("Hozene cislo je: " + hozeneCislo);
+
+        if (hozeneCislo % 2 == 1) {
+            System.out.println("Licha - Vyhravas!");
+        }
+        else {
+            System.out.println("Suda - Prohravas!");
+        } while (ridiciPromenna);
 
 
 
